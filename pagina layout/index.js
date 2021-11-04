a9d413de931f5ca35ac59b1a7b7732b18930e2a1
Xaviermora <xaviermoravega12@gmail.com>
2021-11-04 13:56:59 -0300

    avance

--- pagina layout/index.js
+++ pagina layout/index.js
@@ -2,6 +2,7 @@ const img = document.getElementsByClassName('ampliar-img');
 const containerImgAmpliada = document.getElementById('container-img-ampliada');
 const body = document.getElementsByTagName('body');
 const resultadoImgAmpliada = document.getElementById('img-zoom');
+const navbar = document.getElementsByClassName("navbar");
 
 for(let i=0; i<img.length; i++){
     img[i].addEventListener('click', ampliarImg);
@@ -14,10 +15,13 @@ function ampliarImg(e){
     containerImgAmpliada.classList.remove('d-none');
     containerImgAmpliada.classList.add('d-flex');
     body[0].style.overflowY = "hidden";
+    navbar[0].classList.add('d-none');
+    event.preventDefault();
 }
 
 function imgAmpliadaCerrar(){
     containerImgAmpliada.classList.remove('d-flex');
     containerImgAmpliada.classList.add('d-none');
     body[0].style.overflowY = "";
+    navbar[0].classList.remove('d-none');
 }
